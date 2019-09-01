1460a10a42da69be3afc5586b166dcf4b49a3b9a
John Lyon-Smith <john@lyon-smith.org>
2019-09-01 15:09:31 -0700

    Show warnings on duplicates and missing files in song list

--- src/MakeSetListTool.js
+++ src/MakeSetListTool.js
@@ -1,9 +1,9 @@
 import parseArgs from "minimist"
 import autobind from "autobind-decorator"
 import * as version from "./version"
-import tempy from "tempy"
 import fs from "fs-extra"
-import path from "path"
+import readdirp from "readdirp"
+import { exec } from "promisify-child-process"
 
 const pipeToPromise = (readable, writeable) => {
   const promise = new Promise((resolve, reject) => {
@@ -70,6 +70,57 @@ Options:
 
     if (!pdfRootPath) {
       throw new Error("A PDF root directory must be given")
+    }
+
+    const songList = await fs.readFile(songListPath, { encoding: "utf8" })
+    const songMap = new Map()
+
+    for (const title of songList.split("\n")) {
+      songMap.set(title + ".pdf", [])
+    }
+
+    const entries = await readdirp.promise(pdfRootPath, {
+      fileFilter: Array.from(songMap.keys()),
+    })
+    const outputPath = "song-list.pdf"
+
+    for (const entry of entries) {
+      const files = songMap.get(entry.basename)
+
+      if (files) {
+        files.push(entry.fullPath)
+      }
+    }
+
+    for (const [name, files] of songMap.entries()) {
+      const title = name.slice(0, -4)
+
+      if (files.length === 0) {
+        this.log.warning(`Song '${title}' was not found`)
+      } else if (files.length > 1) {
+        this.log.warning(
+          `Song '${title}' has ${files.length} copies: ${files
+            .map((file) => '"' + file + '"')
+            .join(", ")}`
+        )
+      }
+    }
+    const command = `pdf-o-rama concat -o ${outputPath} ${Array.from(
+      songMap.values()
+    )
+      .filter((files) => `"${files[0]}"`)
+      .join(" ")}`
+
+    if (this.debug) {
+      this.log.info(command)
+    }
+
+    this.log.info(`Combining ${outputPath} PDF's...`)
+
+    try {
+      await exec(command)
+    } catch (e) {
+      throw new Error(`Unable to create PDF. ${result.stderr.trim()}`)
     }
 
     this.log.info("Done")
